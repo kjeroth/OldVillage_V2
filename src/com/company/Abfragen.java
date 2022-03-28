41e9e653f0bdf68ada9a8de4bc51107f58bbd68d
OV_V2 <gamer7070@web.de>
2022-03-28 22:21:26 +0200

    1. Einsesen erfolgreich

---
+++ src/com/company/Abfragen.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Abfragen {
+
+    public void chippId(int[] iD, int st){
+        JOptionPane.showMessageDialog(null,"ausgabe");
+        if (st==1){
+            System.out.println(iD);
+
+        }
+    }
+}
