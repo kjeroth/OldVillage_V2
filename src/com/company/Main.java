f2698982281b34465c802b2f614b09ee88a2f246
kjeroth <victor94@web.de>
2022-04-05 19:14:48 +0200

    nutzen eigener Variablen aufgrund v. vererbung über extend der Jeweiliege Button/ zweck muss zugewiesen werden
    größe der Karten angepasst
    
    Eventuel muss was im Handler oder was mit dem event handler gemacht werden...

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,79 +7,7 @@ import java.awt.event.ActionListener;
 
 public class Main extends  JFrame  implements ActionListener {
 
-
-    public int btPressed;
-    public int zahl;
-    public int mainCount;
-    public int[] arrayZahl = new int[10];
-    public boolean mEnter;
-    // Abfragen abfragen =new Abfragen();
-
-
-    public void übergabe(int avg, int count, boolean enter) {
-
-        this.setZahl(avg);
-        this.setMainCount(count);
-        this.setmEnter(enter);
-
-        if (this.getMainCount() < 10) {
-            this.setArrayZahl(this.getMainCount(), this.getZahl());
-            System.out.println("count: " + this.getMainCount() + "zahl " + this.getZahl());
-        }
-        if (this.getMainCount() >= 9 & this.getmEnter()) {
-            for (int value : this.getArrayZahl()) {
-                System.out.print(value);
-                // abfragen.chippId(getArrayZahl());
-
-            }
-            JOptionPane.showMessageDialog(null, getArrayZahl());
-            System.out.println("ende");
-            // an dieser Stelle soll das Array an getChippId übergeben werden
-            // oder direkt an chippID--> ausder eingebundenen klasse Abfragen in der Klasse Chipp
-            //   chip1.chippId(this.arrayZahl,this.getBtPressed());
-        }
-    }
-
-    public  void setmEnter(boolean avg){
-        this.mEnter=avg;
-    }
-    public boolean getmEnter(){
-        return this.mEnter;
-    }
-
-    public void setArrayZahl(int avg, int zahl) {
-        arrayZahl[avg] = zahl;
-    }
-
-    public int[] getArrayZahl() {
-        return arrayZahl;
-    }
-
-    public int getZahl() {
-        return zahl;
-    }
-
-    public void setZahl(int avg) {
-        zahl = avg;
-    }
-
-    public int getMainCount() {
-        return mainCount;
-    }
-
-    public void setMainCount(int avg) {
-        mainCount = avg;
-    }
-
-    public int getBtPressed() {
-        return btPressed;
-    }
-
-    public void setBtPressed(int pressed) {
-        this.btPressed = pressed;
-    }
-
-
+    //Jmenü
     private JMenuBar jmb;
 
     private JMenu identifizieren;
@@ -92,32 +20,53 @@ public class Main extends  JFrame  implements ActionListener {
     private JMenuItem zuweisen;
     private JMenuItem löschen;
 
+    //JPanel
     private JPanel karteWilkommen;
     private JPanel karteIdentification;
     private JPanel karteObjektEinfügen;
     private JPanel karteChippLöschen;
     private JPanel karteChippZuweisen;
 
+    //Textfelder
     private JTextField name;
     private JTextField beschreibung;
     private JTextField HidBeschreibung;
     private JTextField Heilung;
 
-    private JRadioButton status;
+    //Label
+    private JLabel labelChip;
 
+    //Bilder
+    ImageIcon iconChipp;
+
+    //Radio Button
+    private JRadioButton rbStatus;
+
+    //Button
     private JButton btnIdentifizieren;
     private JButton btnEnter;
     private JButton btnLöschen;
 
+    //Combo Box
     private JComboBox BoxBerechtiegung;
     private JComboBox boxArt;
     private JComboBox boxWahlLöschen;
     private JComboBox boxLöschenLöchen;
     private JComboBox boxzuweisen;
 
-
+    //Panel und cardlayout
     private JPanel kartenPanel;
     private CardLayout test;
+
+    //Variablen Deklarationen
+    private int btPressed;
+    private int zahl;
+    private int mainCount;
+    private int[] arrayZahl = new int[10];
+    private boolean mEnter;
+
+
+
 
     public static void main(String[] args) {
         // write your code here
@@ -129,12 +78,10 @@ public class Main extends  JFrame  implements ActionListener {
         Handler handler = new Handler();
 
 
-
-
-
         main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         main.pack();
-        main.setSize(500, 500);
+        main.setSize(600, 600);
+
         main.setLayout(new FlowLayout());
 
         main.getContentPane().addKeyListener(handler);
@@ -185,6 +132,7 @@ public class Main extends  JFrame  implements ActionListener {
         main.setJMenuBar(main.jmb);
         main.setVisible(true);
 
+
     }
 
     private void kartedemFrameZuweisen(Container content) {
@@ -200,6 +148,7 @@ public class Main extends  JFrame  implements ActionListener {
         karteWilkommen.setBackground(Color.green);
         karteWilkommen.add(new JButton("Button"));
 
+
         //textfelder erstellen
         name = new JTextField();
         name.setPreferredSize(new Dimension(250, 80));
@@ -207,14 +156,36 @@ public class Main extends  JFrame  implements ActionListener {
         //butten ersetellen
         btnIdentifizieren = new JButton("Identifizieren");
 
+        //radioButton erstellen
+        rbStatus= new JRadioButton("Status");
+        JLabel label =new JLabel("test label");
+
+        //Bilder erstellen
+        iconChipp =new ImageIcon("iconChipp.png");
+
+        //Label ertsellen
+        labelChip =new JLabel("ich bin ein Label");
+        labelChip.setIcon(iconChipp);
+
+
+
         //action listenere hinzufügen
         btnIdentifizieren.addActionListener(this);
 
 
         // ItemIdentification -->Panel Inhalt
         karteIdentification.add(new JLabel("Identifikation"));
+        karteIdentification.setPreferredSize(content.getSize());
         karteIdentification.setBackground(Color.red);
         karteIdentification.add(btnIdentifizieren);
+        karteIdentification.add(rbStatus);
+        karteIdentification.add(labelChip);
+
+        //rbStatus.setSelected(true);
+
+
+
+
 
 
         // karteIdentification.add(name);
@@ -222,6 +193,8 @@ public class Main extends  JFrame  implements ActionListener {
 
         //neues JPanel enthält das CardLayout, enthältdie Karten
         kartenPanel = new JPanel(new CardLayout());
+        kartenPanel.setSize(content.getSize());
+
 
         //wichtig Objekt benötigt string zum Aufrufen
         kartenPanel.add(karteWilkommen, "1");
@@ -237,6 +210,7 @@ public class Main extends  JFrame  implements ActionListener {
         // neues Cardlayout erstellen
         test = new CardLayout();
         test = (CardLayout) (kartenPanel).getLayout();
+
     }
 
     @Override
@@ -249,6 +223,8 @@ public class Main extends  JFrame  implements ActionListener {
         if (e.getSource().equals(insItem)) {
             System.out.println("Item get pressed");
             test.show(kartenPanel, "3");
+            System.out.println("das ist der Chipp"+this.getArrayZahl());
+
         }
         if (e.getSource() == insSkill) {
             System.out.println("skill get pressed");
@@ -265,7 +241,72 @@ public class Main extends  JFrame  implements ActionListener {
             setBtPressed(1);
             this.getContentPane().requestFocus();
             System.out.println("getBtPressed:" + getBtPressed());
-
         }
     }
+
+    public void übergabe(int avg, int count, boolean enter) {
+        this.setZahl(avg);
+        this.setMainCount(count);
+        this.setmEnter(enter);
+
+        if (this.getMainCount() < 10) {
+            this.setArrayZahl(this.getMainCount(), this.getZahl());
+            System.out.println("count: " + this.getMainCount() + "zahl " + this.getZahl());
+        }
+
+        if (this.getMainCount() >= 9 & this.getmEnter()) {
+            for (int value : this.getArrayZahl()) {
+                System.out.print(value);
+            }
+            JOptionPane.showMessageDialog(null, getArrayZahl());
+            System.out.println("ende "+ getBtPressed());            }
+    }
+
+    public JRadioButton getRbStatus() {
+        return rbStatus;
+    }
+
+    public void setRbStatus(JRadioButton rbStatus) {
+        this.rbStatus = rbStatus;
+    }
+    public  void setmEnter(boolean avg){
+        this.mEnter=avg;
+    }
+    public boolean getmEnter(){
+        return this.mEnter;
+    }
+
+    public void setArrayZahl(int avg, int zahl) {
+        arrayZahl[avg] = zahl;
+    }
+
+    public int[] getArrayZahl() {
+        return arrayZahl;
+    }
+
+    public int getZahl() {
+        return zahl;
+    }
+
+    public void setZahl(int avg) {
+        zahl = avg;
+    }
+
+    public int getMainCount() {
+        return mainCount;
+    }
+
+    public void setMainCount(int avg) {
+        mainCount = avg;
+    }
+
+    public int getBtPressed() {
+        return btPressed;
+    }
+
+    public void setBtPressed(int pressed) {
+        this.btPressed = pressed;
+    }
+
+
 }
